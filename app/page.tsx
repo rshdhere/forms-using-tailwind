20e261c5ed26191aa527fd5438069674fddbe946
raashed <mohammedraashed10@gmail.com>
2025-09-03 23:38:42 +0530

    chore: bootstraped form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Form from '@/components/form';
+
 export default function Home() {
-  return <div>hello there</div>;
+  return (
+    <main className="mx-auto flex h-screen max-w-4xl flex-col items-center">
+      <Form />
+    </main>
+  );
 }
